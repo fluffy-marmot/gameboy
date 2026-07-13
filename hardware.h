7864ab51425ab04a3bb20ef80edca8f783ad65e0
fluffy-marmot <10621013+fluffy-marmot@users.noreply.github.com>
2026-07-13 05:24:39 -0700

    initial commit

---
+++ hardware.h
@@ -0,0 +1,47 @@
+#ifndef GB_HARDWARE
+#define GB_HARDWARE
+
+#include <stdint.h>
+
+#define KiB                         1024
+
+#define GB_DMG_WRAM             ( 8 * KiB)
+#define GB_CGB_WRAM             (32 * KiB)
+#define GB_DMG VRAM             ( 8 * KiB)
+#define GB_CGB VRAM             (16 * KiB)
+
+#define GB_DISPLAY_WIDTH            160
+#define GB_DISPLAY_HEIGHT           144
+
+
+typedef struct {
+    union {
+        uint8_t reg[8];
+        struct {
+            uint8_t B;              // 0x00 0b000
+            uint8_t C;              // 0x01 0b001
+            uint8_t D;              // 0x02 0b010
+            uint8_t E;              // 0x03 0b011
+            uint8_t H;              // 0x04 0b100
+            uint8_t L;              // 0x05 0b101
+            union {
+                uint16_t AF;        // unfortunately other pairs have wrong endianness 
+                struct {
+                    uint8_t F;      // z, n, h, c flags (zero, subtraction, half carry, carry)        
+                    uint8_t A;      // 0x06 0b111
+                };
+            };
+        };
+    };
+    uint16_t PC;
+    uint16_t SP;
+    uint8_t IR;                     // instruction register
+    uint8_t IE;                     // interrupt enable
+} gb_cpu_t;
+
+typedef struct {
+    gb_cpu_t cpu;
+    uint8_t *mem;       // TODO
+} gb_gameboy_t;
+
+#endif
